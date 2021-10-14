2e603cbab9aad6419f105ddc838a1a4c656936d9
Qiang Huang <qihuang@microsoft.com>
2021-10-14 10:55:15 +0800

    first commit

---
+++ src/webparts/qiangTest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IQiangTestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'QiangTestWebPartStrings' {
+  const strings: IQiangTestWebPartStrings;
+  export = strings;
+}
